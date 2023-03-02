4630c92b94c4de201183c34da6f066c5154bff35
Ivan Ogloblin <vip.ogloblin@bk.ru>
2023-03-02 22:29:06 +0800

    update

--- Easy_Learn/Startup.cs
+++ Easy_Learn/Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
@@ -12,15 +13,19 @@ namespace Easy_Learn
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        
         public void ConfigureServices(IServiceCollection services)
         {
+            // добавляем поддержку контроллеров и представлений (MVC)
+            services.AddControllersWithViews()
+                // выставляем совместимость с asp.net core 3.0 
+                .SetCompatibilityVersion(CompatibilityVersion.Version_3_0).AddSessionStateTempDataProvider();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            // отслеживание информации об ошибках
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -28,12 +33,13 @@ namespace Easy_Learn
 
             app.UseRouting();
 
+            // подключение поддержки статичных файлов в приложении html css js
+            app.UseStaticFiles();
+
+            // регистрируем нужные нам маршруты (эндпоинты)
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
